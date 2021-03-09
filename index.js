f6772dd524b9b1171c0c43092f76613c88a1fd32
replSakshi <scjuly69@gmail.com>
2021-03-09 06:52:57 +0000

    Update: Two new questions added

--- index.js
+++ index.js
@@ -1,102 +1,112 @@
-var readLineSync=require("readline-sync");
-console.log("Hello! Welcome to FRIEND's QUIZ. You can play alone or with someone else."+"\n"+"Hope you enjoy this!!!"+"\n")
-var score=0;
-var quizList=[
-{
-  question:"What is Ross's first wife name ? ",
-  answer:"carol"
-},
-{
-  question:"What is Pheobe's last name ? ",
-  answer:"buffay"
-},
-{question:"What is Chandler bing's middle name ? ",
-answer:"muriel"}
+var readLineSync = require("readline-sync");
+console.log("Hello! Welcome to FRIEND's QUIZ. You can play alone or with someone else." + "\n" + "Hope you enjoy this!!!" + "\n")
+var score = 0;
+var quizList = [
+  {
+    question: "What is Ross's first wife name ? ",
+    answer: "carol"
+  },
+  {
+    question: "What is Pheobe's last name ? ",
+    answer: "buffay"
+  },
+  {
+    question: "What is Chandler bing's middle name ? ",
+    answer: "muriel"
+  },
+  {
+    question: "Who does Pheobe marry ? ",
+    answer: "mark"
+  },
+  {
+    question: "What was Rachel's dog name who dies ? ",
+    answer: "lapoh"
+  }
 
 ];
 var username;
 
-class UsersList{
-  constructor(){
-    this.usersList=[];
- 
+class UsersList {
+  constructor() {
+    this.usersList = [];
+
   }
 
-  addUser(name,score){
-    this.usersList.push({username:name,score:score});
+  addUser(name, score) {
+    this.usersList.push({ username: name, score: score });
   }
-  printList(){
-      for(let i=0;i<this.usersList.length;i++){
-        console.log("Name------"+" "+this.usersList[i].username);
-        console.log("Your Score------"+" "+this.usersList[i].score)
-      }
-    
+  printList() {
+    for (let i = 0; i < this.usersList.length; i++) {
+      console.log("Name------" + " " + this.usersList[i].username);
+      console.log("Your Score------" + " " + this.usersList[i].score)
+    }
+
   }
-  listLength(){
+  listLength() {
     return this.usersList.length;
   }
-  getUsers(){
+  getUsers() {
     return this.usersList;
 
   }
 }
-function play(question,answer){
-  var userAnswer=readLineSync.question(question);
+function play(question, answer) {
+  var userAnswer = readLineSync.question(question);
 
-  if(userAnswer.toLowerCase() === answer){
+  if (userAnswer.toLowerCase() === answer) {
     console.log("Correct Answer!!" + "\n");
-    
-    score+=1;
+
+    score += 1;
   }
-  else{
+  else {
     console.log("Oops! Wrong Answer" + "\n");
-    console.log("Correct answer is:"+" "+answer)
-   
+    console.log("Correct answer is:" + " " + answer)
+
   }
 
-  console.log("current score:"+" "+ score +"\n")
-  
+  console.log("current score:" + " " + score + "\n")
+
 
 }
-  const user=new UsersList();
-function displayWinner(){
-  const list=user.getUsers();
-  if(list[0].score>list[1].score) console.log('Winner is-------- '+list[0].username);
-  else console.log('Winner is******* '+list[1].username);
+const user = new UsersList();
+function displayWinner() {
+  const list = user.getUsers();
+  if (list[0].score > list[1].score) console.log('Winner is-------- ' + list[0].username);
+  else console.log('Winner is******* ' + list[1].username);
 }
 
 
-function startGame(){
-  userName=readLineSync.question("What is your name ? ");
-  for(let i=0;i<quizList.length;i++){
-    play(quizList[i].question,quizList[i].answer)
+function startGame() {
+  userName = readLineSync.question("What is your name ? ");
+  for (let i = 0; i < quizList.length; i++) {
+    play(quizList[i].question, quizList[i].answer)
   }
-user.addUser(userName,score);
-user.printList();
-if(user.listLength()>=2) displayWinner();
-else {
-  console.log("-----Player 1 Game Over-----"+"\n");
- const player2=readLineSync.question("Player 2 ready to begin ? ");
- if(player2.toLowerCase()==="yes"){
-   freshGame();
+  user.addUser(userName, score);
+  user.printList();
+  if (user.listLength() >= 2) displayWinner();
+  else {
+    console.log("-----Player 1 Game Over-----" + "\n");
+    const player2 = readLineSync.question("Player 2 ready to begin ? ");
+    if (player2.toLowerCase() === "yes") {
+      freshGame();
 
- }
- else return;
+    }
+    else return;
 
-}
+  }
 }
 
-function freshGame(){
-  score=0;
-  var basicAnswer=readLineSync.question("Do you watch Friends ? ");
-if(basicAnswer.toLowerCase() === "yes"){
-  startGame();
-  
-}
-else{
-  console.log("Sorry! This is only for Friends Fans!!")
-  
-}
+function freshGame() {
+  score = 0;
+  var basicAnswer = readLineSync.question("Do you watch Friends ? ");
+  if (basicAnswer.toLowerCase() === "yes") {
+    startGame();
+
+  }
+  else {
+    console.log("Sorry! This is only for Friends Fans!!")
+
+  }
 }
 freshGame();
 
